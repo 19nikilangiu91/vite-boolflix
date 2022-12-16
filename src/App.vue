327f4aafb9a218e0f3746a340d365d6ae2509c00
19nikilangiu91 <niki.langiu@gmail.com>
2022-12-16 14:09:44 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<template>
+  <HelloWorld />
+</template>
+
+<style lang="scss" >
+@use './style/general.scss' as*;
+@use './style/partials/variables' as*;
+
+h1 {
+  color: $primary;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<script>
+
+export default {
+  name: "Helloworld"
+}
+
+</script>
+
+<template>
+  <h1>Hello to everyone!</h1>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
