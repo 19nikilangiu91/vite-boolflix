f96c97f611bf66f1e40b5243e57d07996bc0c752
19nikilangiu91 <niki.langiu@gmail.com>
2022-12-17 16:04:59 +0100

    Created AppSearch.vue

---
+++ src/components/AppSearch.vue
@@ -0,0 +1,35 @@
+<script>
+
+// Importo il file "store.js"
+import { store } from '../store.js';
+
+export default {
+    name: "AppSearch",
+    data() {
+        return {
+            store,
+        }
+    }
+
+}
+
+</script>
+
+<template>
+    <div>
+        <div>
+            <label for="search-character"></label>
+            <input class="form-control" type="text" id="search-character" placeholder="Search Character"
+                v-model="store.searchText">
+        </div>
+        <div>
+            <button type="submit" class="btn btn-primary" @click.prevent="$emit('search')">Search</button>
+        </div>
+
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
