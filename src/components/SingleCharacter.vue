a93495095af9d0d661b8c351cc0f7396e5515234
19nikilangiu91 <niki.langiu@gmail.com>
2022-12-20 13:39:44 +0100

    Milestone 4 Completed

--- src/components/SingleCharacter.vue
+++ src/components/SingleCharacter.vue
@@ -4,7 +4,7 @@ import { store } from '../store.js';
 
 export default {
     name: "SingleCharacter",
-    props: ["movie"],
+    props: ["info"],
     data() {
         return {
             store,
@@ -35,9 +35,9 @@ export default {
         }
     },
     methods: {
-        // Creo un metodo "creaPathImg" per concatenare l'immagine.
+        // Milestone 3 - Creo un metodo "creaPathImg" per concatenare l'immagine.
         creaPathImg() {
-            return this.store.urlImage + "w342" + this.movie.poster_path;
+            return this.store.urlImage + "w342" + this.info.poster_path;
         },
         // Milestone 3 - Creo un metodo per trasformare il voto da 1 a 10 decimale in un numero intero da 1 a 5.
         starsVote(vote) {
@@ -50,7 +50,7 @@ export default {
         // Creo un metodo "getFlags" per far apparire le bandiere in maniera dinamica.
         getFlags() {
             for (let i = 0; i < this.flagsArray.length; i++) {
-                if (this.movie.original_language === this.flagsArray[i].language) {
+                if (this.info.original_language === this.flagsArray[i].language) {
                     return this.flagsArray[i].image;
                 }
             }
@@ -68,38 +68,73 @@ export default {
     <!-- Qui importerò in "SingleCharacter" -->
     <div class="singlecharacter">
         <div>
-            <img :src="creaPathImg()">
-        </div>
-        <!-- Creo un v-if per i "Movie" e per le "Tv Series" -->
-        <div
-            v-if="movie.title !== '' && movie.original_title !== '' && movie.name !== '' && movie.original_name !== ''">
-            <div>Titolo:{{ movie.title }} {{ movie.name }}</div>
-            <div>Titolo Originale:{{ movie.original_title }} {{ movie.original_name }}</div>
-        </div>
-        <div>
-            <!-- Richiamo il metodo "getFlags" -->
-            <img :src="getFlags" alt="" class="flag">
-        </div>
-        <div>Voto:{{ movie.vote_language }}
-            <!-- Richiamo il metodo "starsVote" -->
-            <i v-for="star in starsVote(movie.vote_average)" :key="star">
-                <font-awesome-icon icon="fa-solid fa-star" />
-            </i>
+            <img class="image" :src="creaPathImg()" onerror="this.src= 'image/imagenotfound.webp'">
+            <div class="content">
+                <!-- Creo un v-if per i "Movie" e per le "Tv Series" -->
+                <!-- Milestone 4 - Sposto il contenuto all'interno della carta-->
+                <div
+                    v-if="info.title !== '' && info.original_title !== '' && info.name !== '' && info.original_name !== ''">
+                    <div>Titolo:{{ info.title }} {{ info.name }}</div>
+                    <div>Titolo Originale:{{ info.original_title }} {{ info.original_name }}</div>
+                    <!-- Richiamo il metodo "getFlags" -->
+                    <div>Language:
+                        <img :src="getFlags" alt="flag" class="flag">
+                    </div>
+                    <div>Voto:{{ info.vote_language }}
+                        <!-- Richiamo il metodo "starsVote" -->
+                        <i v-for="star in starsVote(info.vote_average)" :key="star" class="stars">
+                            <font-awesome-icon icon="fa-solid fa-star" />
+                        </i>
+                    </div>
+                    <div>Overview:{{ info.overview }}
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
+@use 'src/style/partials/variables' as*;
+
 .singlecharacter {
     margin: 10px;
+    position: relative;
+    ;
 
-    div {
-        font-size: 15px;
+    .image {
+        width: 342px;
+        height: 513px;
     }
 
-    .flag {
-        width: 10%;
-        height: 20px;
+    .content {
+        overflow: scroll;
+        width: 342px;
+        height: 513px;
+        padding: 30px;
+        top: 0;
+        position: absolute;
+        font-size: 0px;
+
+        .flag {
+            width: 0%;
+            height: 0px;
+        }
+
+        &:hover {
+            font-size: 20px;
+
+            background-color: $third;
+
+            .flag {
+                width: 10%;
+                height: 20px;
+            }
+        }
+
+        .stars {
+            color: $sixth;
+        }
     }
 }
 </style>
